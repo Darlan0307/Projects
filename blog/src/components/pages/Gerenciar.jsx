7573f32a5de94c2299a6c0c14e91cd3dedbfc33f
Darlan Martins <122477884+Darlan0307@users.noreply.github.com>
2023-12-10 20:40:26 -0300

    novas funcionalidades de editar e remover

--- blog/src/components/pages/Gerenciar.jsx
+++ blog/src/components/pages/Gerenciar.jsx
@@ -2,10 +2,27 @@ import React, { useContext } from 'react'
 import { PostsContext } from '../context/PostsContext'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from 'react-router-dom';
 
 const Gerenciar = () => {
 
   const {posts,setPosts} = useContext(PostsContext)
+
+  const navigate = useNavigate()
+
+  const handleDelete = (id) => {
+    const escolha = confirm("Deseja realmente excluir esse post?")
+
+    if(escolha){
+      const newPosts = posts.filter(post => post.id !== id)
+      setPosts(newPosts)
+    }
+
+  }
+
+  const handleEdit = (id) => {
+    navigate(`/editpost/${id}`)
+  }
 
   return (
     <div className="gerenciar">
@@ -18,8 +35,12 @@ const Gerenciar = () => {
               <h2>{post.title}</h2>
               
               <div className='config_buttons'>
-                <button className='edit_button'><EditIcon sx={{ fontSize:30,color:"#242424" }}/></button>
-                <button className='remove_button'><DeleteIcon sx={{ fontSize:30,color:"#242424" }}/></button>
+                <button 
+                onClick={()=>handleEdit(post.id)}
+                className='edit_button'><EditIcon sx={{ fontSize:30,color:"#242424" }}/></button>
+                <button 
+                onClick={()=>handleDelete(post.id)}
+                className='remove_button'><DeleteIcon sx={{ fontSize:30,color:"#242424" }}/></button>
               </div>
             </article>
             
--- blog/src/components/pages/UpdatePost.jsx
+++ blog/src/components/pages/UpdatePost.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { PostsContext } from '../context/PostsContext'
 
 const UpdatePost = () => {
+
+  const {posts,setPosts} = useContext(PostsContext)
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [postActive,setPostActive] = useState(posts.filter(post => post.id == id)[0] || "")
+  
+  const handleEdit = () => {
+    const values = Object.values(postActive)
+
+    if(!values.includes(null)){
+      const newPosts = posts.filter(post => post.id != id)
+
+      newPosts.unshift(postActive);
+
+      setPosts(newPosts)
+
+      navigate('/')
+      alert("Post atualizado com sucesso!");
+    }
+  }
+
   return (
     <div className="form_newpost">
       <div>
         <label htmlFor="title">Titulo:</label>
         <input type="text" id="title"
-        onChange={(e)=> setNewPost({
-          ...newPost,
-          title:e.target.value
+        value={postActive.title}
+        onChange={(e)=> setPostActive({
+          ...postActive,
+          title: e.target.value
         })}
         />
       </div>
       <div>
         <label htmlFor="text">Texto:</label>
         <textarea id="text"
-        onChange={(e)=> setNewPost({
-          ...newPost,
-          body:e.target.value
+        value={postActive.body}
+        onChange={(e)=> setPostActive({
+          ...postActive,
+          body: e.target.value
         })}
         ></textarea>
       </div>
       <button
       className="button"
-      onClick={()=>{
-        addNewPost()
-        
-      }}
+      onClick={handleEdit}
       >Atualizar</button>
     </div>
   )
