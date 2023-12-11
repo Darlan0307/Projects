d14ca59738a7b9f8c0c0771fd82ac91aff1da3fd
Darlan Martins <122477884+Darlan0307@users.noreply.github.com>
2023-12-11 11:04:34 -0300

    salvando os novos posts no localstorage

--- blog/src/components/context/PostsProvider.jsx
+++ blog/src/components/context/PostsProvider.jsx
@@ -1,32 +1,43 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { PostsContext } from "./PostsContext";
 import axios from "axios";
 
-export const PostsProvider = ({children}) => {
+export const PostsProvider = ({ children }) => {
+  const [posts, setPosts] = useState([]);
+  const [postsLoaded, setPostsLoaded] = useState(false);
 
-    const [posts,setPosts] = useState([]);
-
-    const fetchData = async () => {
-        try{
-        
-            const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-
-            setPosts(response.data)
-
-        }catch(err){
-            console.log("ERROR:",err);
-        }
-        
+  // Efeito para recuperar os posts do localStorage ao iniciar a aplicação
+  useEffect(() => {
+    const storedPosts = localStorage.getItem("posts");
+    if (storedPosts) {
+      setPosts(JSON.parse(storedPosts));
+      setPostsLoaded(true);
+    } else {
+      fetchData(); // Se não houver posts no localStorage, busca da API
     }
+  }, []);
 
-    useEffect(()=>{
-        fetchData();
-    },[])
+  // Efeito para salvar os posts no localStorage sempre que o estado for alterado
+  useEffect(() => {
+    if (postsLoaded) {
+      localStorage.setItem("posts", JSON.stringify(posts));
+    }
+  }, [posts, postsLoaded]);
 
-    return (
-        <PostsContext.Provider value={{posts,setPosts}}>
-            {children}
-        </PostsContext.Provider>
-    )
+  // Função para buscar os posts da API
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(response.data);
+      setPostsLoaded(true);
+    } catch (err) {
+      console.log("ERROR:", err);
+    }
+  };
 
-}
+  return (
+    <PostsContext.Provider value={{ posts, setPosts }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
